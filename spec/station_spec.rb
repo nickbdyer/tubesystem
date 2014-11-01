d04b63ad7415cdc4fa6463109407011136001bef
Nick Dyer <nbdyer@gmail.com>
2014-11-01 18:33:55 +0000

    Tidy station_spec

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -7,25 +7,19 @@ describe Station do
   let(:passenger)   { double :passenger }
 
   it "should be empty of trains when initialized" do
-    bond_street = Station.new
     expect(bond_street.train_count).to eq 0
   end
 
   it "should be empty of passengers when initialized" do
-    bond_street = Station.new
     expect(bond_street.passengers).to eq 0
   end
 
   it "should accept a train at the station" do
-    bond_street = Station.new
-    train = double :train
     bond_street.stop(train)
     expect(bond_street.train_count).to eq 1
   end
 
   it "should accept a passenger at the station" do
-    bond_street = Station.new
-    passenger = double :passenger
     bond_street.accept(passenger)
     expect(bond_street.passenger_count).to eq 1
   end
