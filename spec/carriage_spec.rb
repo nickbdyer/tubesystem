9785218bb625125a5ed3968f6b0895861de1eb24
Nick Dyer <nbdyer@gmail.com>
2014-11-01 12:20:58 +0000

    Refactor variables using let

--- spec/carriage_spec.rb
+++ spec/carriage_spec.rb
@@ -2,26 +2,24 @@ require 'carriage'
 
 describe Carriage do
 
+  let(:carriage)  { Carriage.new      }   
+  let(:passenger) { double :passenger }
+
   it "should be empty when initialized" do
-    carriage = Carriage.new
     expect(carriage).to be_empty
   end
 
   it "should allow a passenger to board" do
-    carriage = Carriage.new
-    passenger = double :passenger
     carriage.board(passenger)
     expect(carriage).not_to be_empty
   end
 
 
-it "should allow a passenge to alight" do
-  carriage = Carriage.new
-  passenger = double :passenger
-  carriage.board(passenger)
-  carriage.alight(passenger)
-  expect(carriage).to be_empty
-end
+  it "should allow a passenge to alight" do
+    carriage.board(passenger)
+    carriage.alight(passenger)
+    expect(carriage).to be_empty
+  end
 
 
 end
